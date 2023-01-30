a16b644632513067ec388254a2648d56686fcd9c
Joel <100006388+joelrico@users.noreply.github.com>
2023-01-29 20:56:33 -0800

    increased test to 1000

--- trends.py
+++ trends.py
@@ -39,7 +39,7 @@ def read_data(file: str, messages: list ):
     count = 0
     for message in data['messages']:
         
-        if count == 650:
+        if count == 1000:
             break
         
         text = str(message['text']).strip()
