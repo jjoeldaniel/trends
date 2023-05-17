1d585bf342dc64658db20dd4b4d14a66fcdf45f5
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-05-16 23:40:51 -0700

    now outputs neatly to json

--- src/trends.py
+++ src/trends.py
@@ -4,7 +4,6 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import time
-import re
 from sentence_transformers import SentenceTransformer, util
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
@@ -24,8 +23,6 @@ def strip_text(text: str) -> str:
 
     text = text.strip()
 
-    
-
     # Remove stopwords
     stop_words = set(stopwords.words('english'))
     word_tokens = word_tokenize(text)
@@ -38,11 +35,8 @@ def strip_text(text: str) -> str:
     return lemmatize_text(text)
 
 
-def group_messages(json_file_path) -> list[str]:
+def group_messages(json_file_path) -> list[(str, str)]:
     """Loads and groups messages from a JSON file.
-
-    Each index is a string of messages from a given hour
-    with each message separated by a newline character.
 
     Returns:
         list[str]: List of messages grouped by hour
@@ -66,11 +60,11 @@ def group_messages(json_file_path) -> list[str]:
         # Get the text of the current message
         text = strip_text(message["text"])
 
-        # Skip empty messages
-        if text == "":
+        # Skip empty and one-worded messages
+        if text == "" or len(text.split(' ')) == 1:
             continue
 
-        message_to_be_added = (text, authors[message["author_id"]])
+        message_to_be_added = (authors[message["author_id"]], text)
         data.append(message_to_be_added)
 
     return data
@@ -95,27 +89,27 @@ def lemmatize_text(text) -> str:
     return " ".join(lemmatized_text)
 
 
-def build_history(messages: list[(str, str)]) -> list[(str, str)]:
+def build_history(messages: list[(str, str)]) -> list[list[(str, str)]]:
     conversations = []
     current_conversation = []
     i = 0
 
     while i < len(messages) - 1:
-        embeddings1 = model.encode(messages[i][0], convert_to_tensor=True)
-        current_conversation.append(messages[i][0])
+        embeddings1 = model.encode(messages[i][1], convert_to_tensor=True)
+
+        if (messages[i][0], messages[i][1]) not in current_conversation:
+            current_conversation.append((messages[i][0], messages[i][1]))
 
         k = i + 1
-        embeddings2 = model.encode(messages[k][0], convert_to_tensor=True)
+        embeddings2 = model.encode(messages[k][1], convert_to_tensor=True)
         cosine_score = util.cos_sim(embeddings1, embeddings2)
 
         # Threshhold
         if cosine_score >= 0.55:
-            current_conversation.append(messages[k])
+            current_conversation.append((messages[k][0], messages[k][1]))
         else:
-            # Save conversation
-            if len(current_conversation) > 1:
-                conversations.append(current_conversation.copy())
-                current_conversation.clear()
+            conversations.append(current_conversation.copy())
+            current_conversation.clear()
 
         i += 1
 
@@ -125,23 +119,34 @@ def build_history(messages: list[(str, str)]) -> list[(str, str)]:
 def main(file: str):
     start = time.time()
 
-    # Messages
-    messages = group_messages(file)
-
-    history = build_history(messages)
+    # Output data
     json_data = {}
 
-    for i, conversation in enumerate(history):
-        for message in conversation:
-            # if i not in json_data:
-            #     json_data[i] = [message[0]]
-            # else:
-            #     json_data[i] += [message[0]]
-            print(message)
+    # List of pairs (author, message)
+    messages = group_messages(file)
 
-    # file_name = file.replace('test', 'output')
-    # with open(file_name, 'w') as f:
-    #     f.write(json.dumps(json_data, indent=4))
+    # List of conversations (which are list[(str, str)])
+    history = build_history(messages)
+
+    # Iterate through all conversations
+    for i, conversation in enumerate(history):
+
+        # Each index of json_data is an array of messages with a
+        # author and text field
+
+        author_messages = []
+        for messages in conversation:
+            x = {}
+            x['author'] = messages[0]
+            x['message'] = messages[1]
+            author_messages.append(x)
+
+        json_data[i] = author_messages
+
+    # Write to output file
+    file_name = file.replace('test', 'output')
+    with open(file_name, 'w') as f:
+        f.write(json.dumps(json_data, indent=4))
 
     print(f"\nTime Elapsed: {round(time.time() - start, 3)}")
 
