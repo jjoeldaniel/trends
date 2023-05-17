89dcdef9a25937186486b37810aa7d92d5b05c28
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-05-16 22:13:57 -0700

    further progress on conversation modeling

--- src/trends.py
+++ src/trends.py
@@ -1,53 +1,41 @@
 import json
 import nltk
 from nltk.stem import WordNetLemmatizer
-from bertopic import BERTopic
-import time
-import emoji
-import dateutil.parser as dp
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import time
 import re
+from sentence_transformers import SentenceTransformer, util
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
 # Download NLTK resources (if not already downloaded)
 nltk.download("punkt")
 nltk.download("averaged_perceptron_tagger")
 nltk.download("stopwords")
 
-# Load BERTopic model
-topic_model = BERTopic()
-
 
 def strip_text(text: str) -> str:
     """Strips text of non-essential characters.
-
-    Performs the following operations:
-        - Remove emojis
-        - Remove URLs
-        - Remove HTML tags
-        - Remove punctuation
 
     Returns:
         str: Cleaned text
     """
 
-    text = emoji.demojize(text).strip()
+    text = text.strip()
 
-    text = re.sub(r"http\S+", "", text)
-    text = re.sub(r"<\S+", "", text)
-    text = re.sub(r":\S+", "", text)
-    text = re.sub(r"__\S+", "", text)
-    text = re.sub(r"[^\w\s]", "", text)
-
-    text = lemmatize_text(text)
+    
 
     # Remove stopwords
-    text_tokens = word_tokenize(text)
-    tokens_without_sw = [
-        word for word in text_tokens if word not in stopwords.words()]
-    text = " ".join(tokens_without_sw)
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(text)
+    text = " ".join(
+        [w for w in word_tokens if not w.lower() in stop_words])
 
-    return text.strip()
+    if len(text.split()) <= 1:
+        text = ""
+
+    return lemmatize_text(text)
 
 
 def group_messages(json_file_path) -> list[str]:
@@ -62,14 +50,16 @@ def group_messages(json_file_path) -> list[str]:
 
     # Load the JSON file into a list of dictionaries
     with open(json_file_path, "r") as f:
-        messages = json.load(f)["messages"]
+        file_data = json.load(f)
+        author_data = file_data["authors"]
+        messages = file_data["messages"]
 
-    # Get the timestamp of the first message in epoch time
-    current_time = dp.parse(messages[0]["timestamp"]).timestamp()
-    current_messages = []
+    authors = {}
+    data = []
 
-    # Initialize the array to store the grouped messages
-    grouped_messages = []
+    # Build id : username association
+    for author in author_data:
+        authors[author] = author_data[author]["name"]
 
     # Iterate through the remaining messages
     for message in messages:
@@ -80,26 +70,10 @@ def group_messages(json_file_path) -> list[str]:
         if text == "":
             continue
 
-        # Get the timestamp of the current message as a datetime object
-        timestamp = dp.parse(message["timestamp"]).timestamp()
+        message_to_be_added = (text, authors[message["author_id"]])
+        data.append(message_to_be_added)
 
-        # If the current message is within an hour of the previous message,
-        # add it to the current group
-        if abs(timestamp - current_time) < 3600:
-            current_messages.append(text)
-        # Otherwise, add the current group to the grouped messages array
-        # and start a new group
-        else:
-            grouped_messages.append("\n".join(current_messages))
-            current_messages = [text]
-
-        # Update the current time
-        current_time = timestamp
-
-    # Add the last group to the grouped messages array
-    grouped_messages.append("\n".join(current_messages))
-
-    return grouped_messages
+    return data
 
 
 def lemmatize_text(text) -> str:
@@ -121,20 +95,55 @@ def lemmatize_text(text) -> str:
     return " ".join(lemmatized_text)
 
 
+def build_history(messages: list[(str, str)]) -> list[(str, str)]:
+    conversations = []
+    current_conversation = []
+    i = 0
+
+    while i < len(messages) - 1:
+        embeddings1 = model.encode(messages[i][0], convert_to_tensor=True)
+        current_conversation.append(messages[i][0])
+
+        k = i + 1
+        embeddings2 = model.encode(messages[k][0], convert_to_tensor=True)
+        cosine_score = util.cos_sim(embeddings1, embeddings2)
+
+        # Threshhold
+        if cosine_score >= 0.55:
+            current_conversation.append(messages[k])
+        else:
+            # Save conversation
+            if len(current_conversation) > 1:
+                conversations.append(current_conversation.copy())
+                current_conversation.clear()
+
+        i += 1
+
+    return conversations
+
+
 def main(file: str):
     start = time.time()
 
     # Messages
     messages = group_messages(file)
 
-    # Fit the topic model
-    # topics, _ = topic_model.fit_transform(messages)
+    history = build_history(messages)
+    json_data = {}
 
-    end = time.time()
-    print(f"\nTime Elapsed: {round(end - start, 3)}\n")
+    for i, conversation in enumerate(history):
+        for message in conversation:
+            # if i not in json_data:
+            #     json_data[i] = [message[0]]
+            # else:
+            #     json_data[i] += [message[0]]
+            print(message)
 
-    for m in messages:
-        print(m + "\n")
+    # file_name = file.replace('test', 'output')
+    # with open(file_name, 'w') as f:
+    #     f.write(json.dumps(json_data, indent=4))
+
+    print(f"\nTime Elapsed: {round(time.time() - start, 3)}")
 
 
 if __name__ == "__main__":
